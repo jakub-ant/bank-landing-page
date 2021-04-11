145f86cb07f5a40325b49690438e44722677eae3
jakub_ant <jakub.antosiewicz.1998@gmail.com>
2021-04-11 15:19:18 +0200

    Add loan-calculator component

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import {Header} from './scripts/header.js'
 import {OfferOverview} from './scripts/offer-overview.js'
-
-const header = new Header()
-const offerOverview = new OfferOverview()
+import { LoanCalculator} from './scripts/loan.calculator.js'
+const header = new Header();
+const offerOverview = new OfferOverview();
+const  loanCalculator = new  LoanCalculator();
---
+++ scripts/loan.calculator.js
@@ -0,0 +1,34 @@
+export class LoanCalculator {
+    loanSlider = document.querySelector('#loan-slider');
+    loanValue = document.querySelector('#loan-value');
+    loanPeriod = document.querySelector('#loan-period');
+    monthlyPayment = document.querySelector('#monthly-payment')
+    maxValue = 30000;
+    percentOfTheTotalValue = 0;
+    interestRate = 0.9;
+    constructor() {
+        this.setListeners()
+    }
+
+    setListeners() {
+        this.loanSlider.addEventListener('input', () => {
+            this.percentOfTheTotalValue = this.loanSlider.value;
+            this.loanValue.value = (this.maxValue * this.percentOfTheTotalValue) / 100
+            this.monthlyPayment.value = this.countPayment()
+        })
+    }
+
+    countPayment() {
+        const monthlyInterestRate = (this.interestRate / 12) + 1;
+        const loanPeriod = +this.loanPeriod.value;
+        let loanValue = +this.loanValue.value;
+        let totalLoanValue = 0
+        for (let index = 0; index < loanPeriod; index++) {
+            loanValue = loanValue * monthlyInterestRate;
+            const monthlyPayment = loanValue / (loanPeriod - index);
+            totalLoanValue += monthlyPayment;
+            loanValue -= monthlyPayment;
+        }
+        return (totalLoanValue / loanPeriod).toFixed()
+    }
+}
